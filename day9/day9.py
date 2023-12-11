e1a165d85fa2805b2897cbd094f777f81abee952
Kilian Glaus <kilian.glaus@kgk-cgc.ch>
2023-12-11 16:42:56 +0100

    day9 Part I finish

--- day9/day9.py
+++ day9/day9.py
@@ -1,8 +1,33 @@
 
-with open('sample.txt', 'r') as f:    
-    for row in f:
-        data = f.readline().splitlines()
-        print(data)
-        print(data[0] + [1])
+import statistics
+
+def search(data=[]):
+    counter = 0
+    for y in range(len(data)-1):
+        if data[y] == data[y+1]:
+            counter += 1
+
+    if (counter == len(data)-1):
+        return data[len(data)-1]
+    else:
+        dataN=[]
+        for x in range(0, len(data)-1, 1): 
+            dataN.append(-data[x]+data[x+1])
+        #print(dataN)
+        return search(dataN)+data[len(data)-1]
+
+with open('input.txt', 'r') as f:
+    resultat=0   
+    for line in f:
+        dataN=[]
+        data = line.split()
+        #Werte als Zahl in Liste verpacken
+        for x in data:
+            dataN.append(int(x))
+        resultat += int(search(dataN))
+        print("Value = " + str(resultat))
+
+
+        
 
 
