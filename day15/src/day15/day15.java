eb2db90308203985e3e07365b0d775ee15ca201b
Kilian Glaus <kilian.glaus@hotmail.com>
2023-12-17 14:46:43 +0100

    day15 PartII finish

--- day15/src/day15/day15.java
+++ day15/src/day15/day15.java
@@ -3,23 +3,60 @@ package day15;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class day15 {
 
     public static void main(String[] args) throws IOException {
-	try (BufferedReader br = new BufferedReader(new FileReader("sample.txt"))) {
+	
+	    //PartII
+	HashMap<Integer, LinkedHashMap<String, Integer>> boxes = new HashMap<Integer, LinkedHashMap<String, Integer>>();
+	
+	try (BufferedReader br = new BufferedReader(new FileReader("input.txt"))) {
 	    String line = br.readLine();
 	    int totalSum = 0;
 	    while(line != null) {
 	        String[] snippets = line.split(",");
-	        System.out.println(snippets[0]);
 	        for (int i = 0; i < snippets.length; i++) {
-	    	totalSum += (hasher(snippets[i]));
+	            int hashValue = hasher(snippets[i]);
+	            totalSum += hashValue;
+	            String label = label(snippets[i]);
+	            int hashValueII = hasher(label);
+	            // = Setzen oder ersetzen der Werte
+	            if (snippets[i].contains("=")) {
+	        	int focalLength = focalLength(snippets[i]);
+	        	
+	        	if (boxes.get(hashValueII) == null)  {
+	        	    LinkedHashMap<String, Integer> dummy = new LinkedHashMap<>();
+	        	    dummy.put(label, focalLength);
+	        	    boxes.put(hashValueII, dummy);
+	        	}
+	        	else {
+	        	    if(boxes.get(hashValueII).containsKey(label) == true) {
+	        		boxes.get(hashValueII).put(label, focalLength);
+	        	    }
+	        	    else {
+	        		boxes.get(hashValueII).put(label, focalLength);
+	        	    }   
+	        	}
+	            }
+	            // - (Entferenen der Werte)
+	            else {
+	        	if (boxes.get(hashValueII) != null && boxes.get(hashValueII).containsKey(label) == true) {
+	        	    boxes.get(hashValueII).remove(label);
+	        	}
+	            }
+	            
 	        }
 	        line = br.readLine();
 	    }
 	    
-	    System.out.print("Resultat PartI: " + totalSum);
+	    System.out.println("Resultat PartI: " + totalSum);
+	    System.out.println(countingLens(boxes));
 	}
 
     }
@@ -30,6 +67,34 @@ public class day15 {
 	    summe = (summe + word.charAt(i))*17%256;
 	}
 	return summe;
+    }
+    
+    public static int focalLength(String word) {
+	String digit = word.split("=")[1];
+	return Integer.parseInt(digit);
+    }
+    
+    public static String label(String word) {
+	if (word.contains("-")) {
+	    return word.split("-")[0];
+	}
+	else {
+	    return word.split("=")[0];
+	}
+    }
+    
+    public static int countingLens(HashMap<Integer, LinkedHashMap<String, Integer>> boxes) {
+	int totalSum = 0;
+	for(Entry<Integer, LinkedHashMap<String, Integer>> entry : boxes.entrySet()) {
+	    System.out.print(entry);
+	    int counter = 1;
+	    for (Map.Entry<String, Integer> entryII : entry.getValue().entrySet()) {
+		totalSum += entryII.getValue()*counter*(entry.getKey()+1);
+		counter++;
+	    }
+	    System.out.println();
+	}
+	return totalSum;
     }
 
 }
